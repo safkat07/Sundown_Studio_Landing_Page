87b3ff8b4014d55a9cb6700e9d214145111b8b1f
safkat07 <safkatsadli@gmail.com>
2024-03-15 01:26:37 +0600

    featured projects hover effect added

--- src/Components/FeaturedProject/FeaturedProject.jsx
+++ src/Components/FeaturedProject/FeaturedProject.jsx
@@ -1,10 +1,19 @@
 import icon from '../../assets/icon2.png'
+import './FeaturedProject.css'
 const FeaturedProject = () => {
     return (
-        <div className='p-10 pt-28 '>
-            <div className='flex  space-x-2 items-center'>
+        <div className=' pt-28 min-h-screen '>
+            <div className='flex p-10 pb-44 space-x-2 items-center'>
                 <img width={15} src={icon} alt="" />
                 <p className='text-[1vw] '>FEATURED PROJECTS</p>
+            </div>
+
+            {/* List div */}
+            <div>
+                <div id='listCon' >
+                    <div className="overlay"></div>
+                    <h2 className='text-[3vw] relative z-10'>Makers Studio HOI</h2>
+                </div>
             </div>
         </div>
     );
