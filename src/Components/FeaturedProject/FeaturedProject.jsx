1f645d4d37445736433a71c19fa94e904464acf8
safkat07 <safkatsadli@gmail.com>
2024-03-15 03:18:47 +0600

    hover on featured list img activated

--- src/Components/FeaturedProject/FeaturedProject.jsx
+++ src/Components/FeaturedProject/FeaturedProject.jsx
@@ -5,13 +5,27 @@ import './FeaturedProject.css'
 import { useState } from 'react';
 const FeaturedProject = () => {
 
-    const [isEntred, setIsEntred] = useState(false)
+    const [isEntered, setIsEntered] = useState(false);
+    const [bgImage, setBgImage] = useState(null);
+
+    const handleMouseEnter = (imageUrl) => {
+        setIsEntered(true);
+        setBgImage(imageUrl);
+        console.log("bgImage", bgImage);
+    };
+
+    const handleMouseLeave = () => {
+        setIsEntered(false);
+        setBgImage(null);
+    };
 
     return (
         <div className=' pt-44 min-h-screen '>
 
+            {/*  bg-[url('https://www.insplosion.com/blog/wp-content/uploads/2020/04/street-style-in-contemporary-interior-design-insplosion-9-scaled.jpg')] */}
+            {/* bg-[url(${bgImage}')] */}
             {/* fixed image div */}
-            <div id='fixedImg' className={isEntred ? "block fixed" : " hidden fixed"}>
+            <div id='fixedImg'  style={{ backgroundImage: `url(${bgImage})` }}  className={isEntered ? `block fixed  ` : " hidden  fixed"}>
 
 
             </div>
@@ -25,11 +39,14 @@ const FeaturedProject = () => {
 
             {/* List div */}
             <div
-
-                onMouseEnter={() => setIsEntred(true)}
-                onMouseLeave={() => setIsEntred(false)}
+                onMouseEnter={() => setIsEntered(true)}
+                onMouseLeave={() => setIsEntered(false)}
                 className=''>
-                <div id='listCon' data-img='' className='flex justify-between'  >
+                <div id='listCon'
+
+                    onMouseEnter={() => handleMouseEnter('https://www.insplosion.com/blog/wp-content/uploads/2020/04/street-style-in-contemporary-interior-design-insplosion-9-scaled.jpg')} onMouseLeave={handleMouseLeave}
+
+                    className='flex justify-between'  >
                     <div className="overlay"></div>
                     <h2 className='text-[3vw] relative z-10'>SOHO NYC</h2>
                     <div className='flex z-10 items-end flex-col'>
@@ -37,15 +54,18 @@ const FeaturedProject = () => {
                         <p className='opacity-0'>Environment</p>
                     </div>
                 </div>
-                <div id='listCon' className='flex justify-between'>
-                    <div className="overlay" ></div>
+                <div id='listCon' onMouseEnter={() => handleMouseEnter('https://pressbooks.cuny.edu/app/uploads/sites/93/2022/08/thanuj-mathew-8CSTVoDMEXg-unsplash-2048x1365.jpg')} onMouseLeave={handleMouseLeave} className='flex justify-between'>
+                    <div className="overlay"></div>
                     <h2 className='text-[3vw] relative z-10'>Air Force 12021</h2>
                     <div className='flex z-10 items-end flex-col'>
                         <p className='text-left'>AFTERPARTY</p>
                         <p className='opacity-0'>Experiental</p>
                     </div>
                 </div>
-                <div id='listCon' className='flex justify-between' >
+                <div id='listCon'
+
+                    onMouseEnter={() => handleMouseEnter('https://images.unsplash.com/photo-1616046229478-9901c5536a45?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.jpg')} onMouseLeave={handleMouseLeave}
+                    className='flex justify-between' >
                     <div className="overlay"></div>
                     <h2 className='text-[3vw] relative z-10'>SOHO 2023</h2>
                     <div className='flex z-10 items-end flex-col'>
